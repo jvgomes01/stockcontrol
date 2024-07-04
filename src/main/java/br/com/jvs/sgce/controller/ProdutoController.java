75bbefe6f9a26fde985bac7d7c96376d4b2c1d76
Joao Gomes <gomesj.victor@yahoo.com.br>
2024-07-03 21:37:32 -0300

    Criação do modulo que adiciona o produto no estoque

--- src/main/java/br/com/jvs/sgce/controller/ProdutoController.java
+++ src/main/java/br/com/jvs/sgce/controller/ProdutoController.java
@@ -5,6 +5,8 @@ import br.com.jvs.sgce.repository.ProdutosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 
@@ -18,6 +20,20 @@ public class ProdutoController {
         ModelAndView mv = new ModelAndView("ListaProdutos");
         mv.addObject("produtos", produto.findAll());
         return mv;
+    }
+
+    @GetMapping("/adicionarproduto")
+    public ModelAndView adicionar() {
+        ModelAndView mv = new ModelAndView("AdicionarProduto");
+        mv.addObject("produto", new Produto());
+        return mv;
+    }
+
+    @PostMapping("/adicionarproduto")
+
+    public String salvar(@ModelAttribute Produto produto) {
+        this.produto.save(produto);
+        return "redirect:/produtos";
     }
 }
 
